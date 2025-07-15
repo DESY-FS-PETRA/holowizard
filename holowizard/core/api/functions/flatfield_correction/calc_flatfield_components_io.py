334492f5fa22ee68ba94529678848b9ac3552d9f
Johannes Dora <johannes.dora@desy.de>
2025-07-15 15:42:45 +0200

    fix logging

--- holowizard/core/api/functions/flatfield_correction/calc_flatfield_components_io.py
+++ holowizard/core/api/functions/flatfield_correction/calc_flatfield_components_io.py
@@ -12,7 +12,7 @@ from holowizard.core.parameters.flatfield_components_params import FlatfieldComp
 def log_empties(empties):
     i = 0
     for empty in empties:
-        logging.debug("calculate_flatfield_components_input_" + str(i), empty)
+        logging.image_debug("calculate_flatfield_components_input_" + str(i), empty)
         i = i + 1
 
 
