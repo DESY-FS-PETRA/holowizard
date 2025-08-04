54210015968ab1afca46321400a487ac56a28c5a
Johannes Dora <johannes.dora@desy.de>
2025-08-04 12:31:41 +0200

    remove unused and duplicated code

--- holowizard/core/utils/fileio.py
+++ holowizard/core/utils/fileio.py
@@ -24,36 +24,3 @@ def load_multi_img_data(img_files):
 
 def write_img_data(img_file, img_data):
     io.imsave(img_file, np.float32(img_data))
-
-
-def load_motor_log(scan_path):
-    # Returns dict with motor names and positions
-    scanname = scan_path.split("/")[-1]
-    filename = scan_path + "/" + scanname + "__LogMotors.log"
-    lines = [line.rstrip("\n") for line in open(filename)]
-    param_names = lines[0:53]
-    motor_values = lines[58].split("\t")
-    motor_pos = {}
-    for i, string in enumerate(param_names):
-        motorname = string.split(": ")[1]
-        pos = motor_values[i]
-        motor_pos[motorname] = pos
-    return motor_pos
-
-
-def load_scan_log(scan_path):
-    # TODO: needed later for tomo reco
-    pass
-
-
-def load_scan_params(scan_path):
-    scanname = scan_path.split("/")[-1]
-    filename = scan_path + "/" + scanname + "__ScanParam.txt"
-    lines = [line.rstrip("\n") for line in open(filename)]
-    scan_params = {}
-    for i, line in enumerate(lines):
-        if i == 11:
-            scan_params[line.split(" ")[1]] = line.split(" ")[2]
-        else:
-            scan_params[line.split(": ")[0]] = line.split(": ")[1]
-    return scan_params
